7c24305ae452941cc706dab40e43382cfe7403f5
Ricardo_Farias <ricardofariasxyz@gmail.com>
2020-01-18 18:55:56 -0300

    Primeiro commit

---
+++ src/main/java/com/example/carros/domain/Carro.java
@@ -0,0 +1,25 @@
+package com.example.carros.domain;
+
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Carro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String tipo;
+    private String descricao;
+    private String urlFoto;
+    private String urlVideo;
+    private String latitude;
+    private String longitude;
+
+}
